e5a8e346d1bb1160e23595f15d2c133e818a29e3
Rodevp <rodriguezrodrigor12@gmail.com>
2022-03-23 17:27:33 -0500

    creando la clase array

---
+++ arrays/array.py
@@ -0,0 +1,32 @@
+
+
+class Array :
+
+    #                  tamaño  , valor = NONE  
+    def __init__(self, capacity, fill_value) -> None:
+        self.items = []
+        #agregamos el elemento por su capacidad
+        for i in range(capacity) :
+            self.items.append(fill_value)
+
+
+    def __len__(self) :
+        return len(self.items)
+
+    
+    def __str__(self):
+        return str(self.items)
+
+
+    def __iter__(self) :
+        return iter(self.items)
+
+
+    def __getitem__(self, index) :
+        return self.items[index]
+
+    
+    def __setitem__(self, index_value, new_item) :
+        self.items[index_value] = new_item
+
+    
